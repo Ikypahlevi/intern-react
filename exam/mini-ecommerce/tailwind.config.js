a5b7b1d1b3ba70d6747245b9ebdbe2c3f102bc66
duong <duongss2208@gmail.com>
2026-09-13 11:25:42 +0700

    style: increase tailwind text sizes only in config

--- exam/mini-ecommerce/tailwind.config.js
+++ exam/mini-ecommerce/tailwind.config.js
@@ -6,6 +6,16 @@ export default {
   ],
   theme: {
     extend: {
+      fontSize: {
+        'xs': ['0.85rem', { lineHeight: '1.25rem' }],
+        'sm': ['1rem', { lineHeight: '1.5rem' }],
+        'base': ['1.125rem', { lineHeight: '1.75rem' }],
+        'lg': ['1.25rem', { lineHeight: '1.75rem' }],
+        'xl': ['1.4rem', { lineHeight: '2rem' }],
+        '2xl': ['1.65rem', { lineHeight: '2.25rem' }],
+        '3xl': ['2.1rem', { lineHeight: '2.5rem' }],
+        '4xl': ['2.5rem', { lineHeight: '1' }],
+      },
       fontFamily: {
         comic: ['"Bangers"', 'cursive', 'impact', 'sans-serif'],
         bubble: ['"Patrick Hand"', 'cursive', 'sans-serif']
