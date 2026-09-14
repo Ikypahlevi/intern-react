0e4c1cbec213aadf492f948266fd26963e49feee
duong <duongss2208@gmail.com>
2026-09-14 14:55:24 +0700

    chore: clean up temporary scripts and unused files from root directory

--- exam/mini-ecommerce/rewrite_profile_details.cjs
+++
@@ -1,6 +0,0 @@
-﻿const fs = require('fs');
-let c = fs.readFileSync('src/Pages/user/profile/_components/ProfileDetails.jsx', 'utf8');
-
-c = c.replace(/const onSubmit = async \(data\) => \{[\s\S]*?login\(\{/, 'const updateUserMutation = useUpdateUser();\n  const onSubmit = async (data) => {\n    try {\n      setIsSubmitting(true);\n      const currentUser = await userService.getById(user.id);\n      if (!currentUser) {\n        toast.error("Không tìm thấy tài khoản để cập nhật!");\n        return;\n      }\n      const updatedUser = { ...currentUser, ...data };\n      await updateUserMutation.mutateAsync({ id: user.id, ...data });\n      toast.success("Cập nhật thông tin thành công! 🎉");\n      login({');
-
-fs.writeFileSync('src/Pages/user/profile/_components/ProfileDetails.jsx', c);
--- exam/mini-ecommerce/rewrite_user_service.cjs
+++
@@ -1,9 +0,0 @@
-﻿const fs = require('fs');
-let c = fs.readFileSync('src/Services/userService.js', 'utf8');
-
-c = c.replace(
-  'getById: async (id) => {',
-  'getByEmail: async (email) => {\n    return await api.get(/users?email=\);\n  },\n  getById: async (id) => {'
-);
-fs.writeFileSync('src/Services/userService.js', c);
-
